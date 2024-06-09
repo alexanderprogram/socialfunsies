066a37d7b0fbda09e1857402904650bbafa54973
alexanderprogram <98499499+alexanderprogram@users.noreply.github.com>
2024-06-08 19:50:01 -0500

    init of my angular/springboot based SocialFunsies.

---
+++ socialfunsies/src/main/java/com/alexanderland/socialfunsies/SocialfunsiesApplication.java
@@ -0,0 +1,13 @@
+package com.alexanderland.socialfunsies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialfunsiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialfunsiesApplication.class, args);
+	}
+
+}
